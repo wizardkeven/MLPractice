85f615370e30fcef7c9fbbd63a8d39c304471412
GUO KAI <wizardkeven@live.com>
2017-08-24 17:57:14 +0900

    Add py file with input
    A simple input text python script.
     #if entered string starts with 'say', then will output with : You said + the string after 'say' that you entered
     #if entered 'exit' then the program will quit with printing 'I will exit!'
     #else, output: Unknown verb: + the string after 'say' that you entered

---
+++ simple_input.py
@@ -0,0 +1,42 @@
+def get_input():
+    command = input(":").split()
+    verbo_word = command[0]
+    if verbo_word in verb_dict:
+        verb = verb_dict[verbo_word]
+    else:
+        print("Unknown verb {}".format(verbo_word))
+        return
+
+    if len(command) >= 2:
+        noun_word = ''.join(str(e)+' ' for e in command[1:])
+        print(verb(noun_word))
+    elif len(command)==1 and command[0]!='say':
+        verb(command[0])
+    else:
+        print(verb("nothing"))
+
+
+# action
+
+def say(noun):
+    return "You said {}".format(noun)
+
+class Quit_Loop(Exception):
+    pass
+# quit
+def exit(exitComm):
+    print('I will '+exitComm+' !')
+    raise Quit_Loop
+
+# link verb with action
+
+verb_dict = {
+    "say": say,
+    "exit":exit,
+    }
+
+try:
+    while True:
+        get_input()
+except Quit_Loop:
+    pass
