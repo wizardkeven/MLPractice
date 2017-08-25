b93e87c0da780032ad5bf2447fc885b4d3d2cf43
GUO KAI <wizardkeven@live.com>
2017-08-25 11:25:05 +0900

    Add hit and help action binding to goblin
    Type: *hit goblin* to hit the goblin with one point harm
    Type: *help goblin* to help a wounded goblin get one point energy
    Type: *examine goblin* to get current info of the goblin

--- simple_input.py
+++ simple_input.py
@@ -25,6 +25,58 @@ class Goblin(GameObject):
     class_name = "goblin"
     desc= "A fool creature"
 
+    def __init__(self,name):
+        self.class_name = "goblin"
+        self.health = 3
+        self._desc = "A foul creature"
+        super().__init__(name)
+
+    @property
+    def desc(self):
+        if self.health >= 3:
+            return self._desc
+        elif self.health == 2:
+            health_line = "It has a wound on its knee."
+        elif self.health == 1:
+            health_line = "Its left arm has been cut off."
+        elif self.health <= 0:
+            health_line = "It is dead."
+        return self._desc + "\n" + health_line
+
+    @desc.setter
+    def desc(self,value):
+        self._desc = value
+
+# hit goblin with one point harm
+def hit(noun):
+    if noun in GameObject.objects:
+        thing = GameObject.objects[noun]
+        if type(thing) == Goblin:
+            thing.health -= 1
+            if thing.health <= 0:
+                msg = "You killed the goblin!"
+            else:
+                msg = "You hit the {}".format(thing.class_name)
+        else:
+            msg = "I'm not strong enough, I can only hit goblin."
+    else:
+        msg = "There is no {} here.".format(noun)
+    return msg
+
+# help the wounded goblin get one point energy
+def help(noun):
+    if noun in GameObject.objects:
+        thing = GameObject.objects[noun]
+        if type(thing) == Goblin:
+            if thing.health+1<= 3:
+                thing.health+=1
+                msg = "You just help the {} get 1 health!".format(noun)
+            else:
+                msg = "The {} is quite healthy!".format(noun)
+    return msg
+            
+            
+
 # instantiate class
 goblin = Goblin("Gobbly")
 
@@ -47,6 +99,8 @@ verb_dict = {
     "say": say,
     "exit":exit,
     "examine": examine,
+    "hit":hit,
+    "help":help,
     }
 
 
