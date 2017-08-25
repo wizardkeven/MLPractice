f941d9eed72f46216d9f8ebcffc4b43d33b41665
GUO KAI <wizardkeven@live.com>
2017-08-25 10:09:28 +0900

    Add an object and its description funcs
    Add a *Goblin* class with name and self description action.

--- simple_input.py
+++ simple_input.py
@@ -1,3 +1,55 @@
+# class definition zone
+
+class Quit_Loop(Exception):
+    pass
+
+# binding action 
+def exit(exitComm):
+    print('I will {}!'.format(exitComm))
+    raise Quit_Loop
+
+class GameObject:
+    class_name = ""
+    desc = ""
+    objects = {}
+
+    def __init__(self,name):
+        self.name = name
+        GameObject.objects[self.class_name] = self
+
+    def get_desc(self):
+        return self.class_name + "\n"+self.desc
+
+
+class Goblin(GameObject):
+    class_name = "goblin"
+    desc= "A fool creature"
+
+# instantiate class
+goblin = Goblin("Gobbly")
+
+# binding action
+
+def examine(noun):
+    if noun in GameObject.objects:
+        return GameObject.objects[noun].get_desc()
+    else:
+        print(noun)
+        print(GameObject.objects)
+        return "There is no {} here.".format(noun)
+# action
+def say(noun):
+    return "You said {}".format(noun)
+
+
+# link verb with action
+verb_dict = {
+    "say": say,
+    "exit":exit,
+    "examine": examine,
+    }
+
+
 def get_input():
     command = input(":").split()
     verbo_word = command[0]
@@ -8,33 +60,16 @@ def get_input():
         return
 
     if len(command) >= 2:
-        noun_word = ''.join(str(e)+' ' for e in command[1:])
+        noun_word = ''.join(str(e)+' ' for e in command[1:-1])+command[-1]
+        #noun_word = noun_word.rstrip() #strip the last whitespace as the loop above
+                                        #will add  an extra whitespece ate the end of the string
         print(verb(noun_word))
     elif len(command)==1 and command[0]!='say':
         verb(command[0])
     else:
         print(verb("nothing"))
 
-
-# action
-
-def say(noun):
-    return "You said {}".format(noun)
-
-class Quit_Loop(Exception):
-    pass
-# quit
-def exit(exitComm):
-    print('I will {} !'.format(exitComm))
-    raise Quit_Loop
-
-# link verb with action
-
-verb_dict = {
-    "say": say,
-    "exit":exit,
-    }
-
+#main loop
 try:
     while True:
         get_input()
